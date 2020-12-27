95f071bff4b9918e01bfdb8e74e805ad8bcc8cc1
Ben21821 <76652603+Ben21821@users.noreply.github.com>
2020-12-27 22:43:55 +0800

    Update strings.h

--- src/strings.h
+++ src/strings.h
@@ -1,37 +1,37 @@
 ﻿#pragma once
 
-static LPCTSTR en_us[] {
-    "First Person Field of View",
-    "First Person Unarmed Reticle",
-    "First Person Min Pitch Angle",
-    "First Person Max Pitch Angle",
-    "First Person Aiming Field of View",
-    "Third Person Field of View",
-	"Third Person Auto Center",
-    "Third Person Horizontal Origin",
-    "Third Person Vertical Origin",
-    "Third Person Follow Distance",
-    "Third Person Running Shake",
-    "Third Person Min Pitch Angle",
-    "Third Person Max Pitch Angle",
-    "Third Person Aiming Field of View",
-    "Third Person Aiming Horizontal Origin",
-    "Third Person Aiming Follow Distance",
-    "First Person Vehicle Field of View",
-    "First Person Vehicle Vertical Origin",
-    "First Person Vehicle Switch In Water",
-    "First Person Vehicle Switch on Destroyed",
-    "First Person Vehicle Min Pitch Angle",
-    "First Person Vehicle Max Pitch Angle",
-    "Third Person Vehicle Field of View",
-    "Third Person Vehicle Min Pitch Angle",
-    "Third Person Vehicle Max Pitch Angle",
-    "Third Person Vehicle Auto Center",
-    "Third Person Vehicle Follow Distance",
-    "Third Person Vehicle Speed Zoom",
-    "Third Person Vehicle Pivot Scale",
-    "Third Person Vehicle Horizontal Origin",
-    "Third Person Vehicle High Speed Shake"
+static LPCTSTR zh_cn[] {
+    u8"第一人称视野",
+    u8"第一人称无瞄准准星",
+    u8"第一人称最小俯仰角",
+    u8"第一人称最大俯仰角",
+    u8"第一人称瞄准视野",
+    u8"第三人称视野",
+    u8"第三人称自动居中",
+    u8"第三人称横向源点",
+    u8"第三人称垂直源点",
+    u8"第三人称跟随距离",
+    u8"第三人称跑步震动",
+    u8"第三人称最小俯仰角",
+    u8"第三人称最大俯仰角",
+    u8"第三人称瞄准视野",
+    u8"第三人称瞄准横向原点",
+    u8"第三人称瞄准跟随距离",
+    u8"第一人称驾驶视野",
+    u8"第一人称驾驶垂直源点",
+    u8"在水中时切换第一人称驾驶视角",
+    u8"死亡时切换第一人称驾驶视角",
+    u8"第一人称驾驶最小俯仰角",
+    u8"第一人称驾驶最大俯仰角",
+    u8"第三人称驾驶视野",
+    u8"第三人称驾驶最小俯仰角",
+    u8"第三人称驾驶最大俯仰角",
+    u8"第三人称驾驶自动居中",
+    u8"第三人称驾驶跟随距离",
+    u8"第三人称驾驶车辆速度变焦",
+    u8"第三人称驾驶轴心缩放",
+    u8"第三人称驾驶横向源点",
+    u8"第三人称驾驶车辆高速抖动"
 };
 
 static LPCTSTR en_es[] {
@@ -138,6 +138,7 @@ static LPCTSTR* langtext_array[3] {
     en_us,
     en_es,
     ru_ru
+    zh_cn	    
 };
 
 inline LPCTSTR getConstString(int langugageId, GlobalTextEntry_t textId) {
